9a91b7b0ba67fab51b37591065685587d06fbec7
atpons <taiga@atpons.com>
2012-10-13 23:32:16 +0900

    fixed server.rb

--- server.rb
+++ server.rb
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 require "sinatra"
 
-b = []
-
-#open("rss.log") {|file|
-#  a = file.readlines[0]
-#  z = a.to_s
-#  b.push(z)
-#  }
-
 get "/" do
   file = File.lstat("rss.xml").mtime
   @ls = file
